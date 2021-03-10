a95194d5e2340e9525ac4edd8c04f948d99a00b6
Mikhael Dantas <mikhael.dcs@gmail.com>
2021-03-09 23:49:34 -0300

    limiting query

--- src/resolvers/pokemon.resolver.ts
+++ src/resolvers/pokemon.resolver.ts
@@ -1,7 +1,12 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+import {
+	Args,
+	// Mutation,
+	Query,
+	Resolver,
+} from '@nestjs/graphql'
 import Pokemon from '../db/models/pokemon.entity'
 import RepoService from '../repo.service'
-import PokemonInput from './input/pokemon.input'
+// import PokemonInput from './input/pokemon.input'
 
 @Resolver()
 class PokemonResolver {
@@ -9,7 +14,7 @@ class PokemonResolver {
 
 	@Query(() => [Pokemon])
 	public async pokemons(): Promise<Pokemon[]> {
-		return this.repoService.PokemonRepo.find()
+		return this.repoService.PokemonRepo.findAndCount({ take: 10, skip: 0 })[0]
 	}
 
 	@Query(() => Pokemon, { nullable: true })
@@ -17,15 +22,15 @@ class PokemonResolver {
 		return this.repoService.PokemonRepo.findOne(id)
 	}
 
-	@Mutation(() => Pokemon)
-	public async createPokemon(
-		@Args('data') input: PokemonInput,
-	): Promise<Pokemon> {
-		const pokemon = this.repoService.PokemonRepo.create({
-			name: input.name,
-			imageUrl: input.image_url,
-		})
-		return this.repoService.PokemonRepo.save(pokemon)
-	}
+	// @Mutation(() => Pokemon)
+	// public async createPokemon(
+	// 	@Args('data') input: PokemonInput,
+	// ): Promise<Pokemon> {
+	// 	const pokemon = this.repoService.PokemonRepo.create({
+	// 		name: input.name,
+	// 		imageUrl: input.image_url,
+	// 	})
+	// 	return this.repoService.PokemonRepo.save(pokemon)
+	// }
 }
 export default PokemonResolver
